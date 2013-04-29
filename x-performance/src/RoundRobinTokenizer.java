50f0a8c0c313963008b2b2f90666f60363f0649c
Joel Edwards <joeledwards@loadstorm.com>
2013-04-29 15:32:33 -0600

    We now have a base version of RoundRobinTokenizer working.

--- x-performance/src/RoundRobinTokenizer.java
+++ x-performance/src/RoundRobinTokenizer.java
@@ -1,49 +1,64 @@
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class RoundRobinTokenizer extends Thread
 {
 	public static void main(String[] args) throws Exception
 	{
-		int streamCount = 3;
+		int streamCount = 5;
+		String regex = "\\s";
 		String words = "Hi there Bob. How are you doing today. I believe you have my shoes!";
 		ArrayList<ByteArrayOutputStream> streams = new ArrayList<ByteArrayOutputStream>();
 		for (int i = 0; i < streamCount; i++)
 			streams.add(new ByteArrayOutputStream());
-		runTokenizers(words, streams);
+		runTokenizers(words, regex, streams);
 
+		System.out.println("Stream contents:");
 		for (ByteArrayOutputStream stream : streams)
-			System.out.println(stream.toString());
+			System.out.println("  " + stream.toString());
 	}
 
-	public static void runTokenizers(String words, Collection<? extends OutputStream> streams)
+	public static void runTokenizers(String words, String regex, Collection<? extends OutputStream> streams)
 			throws InterruptedException
 	{
 		RoundRobinTokenizer lastTokenizer = null;
 		RoundRobinTokenizer firstTokenizer = null;
 		for (OutputStream stream : streams)
 		{
-			RoundRobinTokenizer tokenizer = new RoundRobinTokenizer(stream);
-			tokenizer.setNextTokenizer(lastTokenizer);
+			RoundRobinTokenizer tokenizer = new RoundRobinTokenizer(stream, regex);
+			if (lastTokenizer != null)
+				lastTokenizer.setNextTokenizer(tokenizer);
 			lastTokenizer = tokenizer;
 			if (firstTokenizer == null)
 				firstTokenizer = tokenizer;
 			tokenizer.start();
 		}
-		firstTokenizer.setNextTokenizer(firstTokenizer);
+		lastTokenizer.setNextTokenizer(firstTokenizer);
 
 		firstTokenizer.tokenize(words);
+
 		firstTokenizer.join();
+		RoundRobinTokenizer nextTokenizer = firstTokenizer.getNextTokenizer();
+		while (nextTokenizer != firstTokenizer) {
+			nextTokenizer.join();
+			nextTokenizer = nextTokenizer.getNextTokenizer();
+		}
 	}
 
+
+	private LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<String>();
 	private OutputStream stream;
+	private String regex;
 	private RoundRobinTokenizer nextTokenizer;
 
-	public RoundRobinTokenizer(OutputStream stream)
+	public RoundRobinTokenizer(OutputStream stream, String regex)
 	{
 		this.stream = stream;
+		this.regex = regex;
 	}
 
 	public void setNextTokenizer(RoundRobinTokenizer nextTokenizer)
@@ -51,13 +66,49 @@ public class RoundRobinTokenizer extends Thread
 		this.nextTokenizer = nextTokenizer;
 	}
 
-	public void run()
-	{
-		// TODO: complete
+	public RoundRobinTokenizer getNextTokenizer() {
+		return nextTokenizer;
 	}
 
 	public void tokenize(String words)
 	{
-		// TODO: complete
+		try {
+			queue.put(words);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+
+	public void log(String message) {
+		System.out.println(getName() + ": " + message);
+	}
+
+	public void run()
+	{
+		String words = "INIT";
+		while (words != null) {
+			try {
+				words = queue.take();
+				String[] parts = words.split(regex, 2);
+				if (parts.length > 0) {
+					log(parts[0]);
+					stream.write(parts[0].getBytes());
+				}
+				if (parts.length > 1) {
+					nextTokenizer.tokenize(parts[1]);
+				}
+				else
+					nextTokenizer.tokenize("");
+				if (words.length() == 0)
+					break;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e);
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage(), e);
+			}
+		}
 	}
 }
