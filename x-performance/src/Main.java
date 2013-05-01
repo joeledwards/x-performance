f20f90d8061b47e6f1907213d8b491d7f12eb8c5
Joel Edwards <joeledwards@gmail.com>
2013-04-30 21:58:44 -0600

    Completed NearestPoints

--- x-performance/src/Main.java
+++ x-performance/src/Main.java
@@ -16,5 +16,7 @@ public class Main
 		//(new LongThreadTest()).test(1024*1024, 8*1024);
 		//(new DoubleCalculation()).test(200*1024*1024, 1024*1024);
 		//(new IntegerCalculation()).test(200*1024*1024, 1024*1024);
+
+		
 	}
 }
--- x-performance/src/NearestPoints.java
+++ x-performance/src/NearestPoints.java
@@ -1,64 +1,143 @@
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.TreeSet;
 
-public class NearestPoints {
-	private static final int POINT_COUNT = 1000;
-	private static final int M_CLOSEST = 5;
+public class NearestPoints
+{
+	private static final int POINT_COUNT = 2000000;
+	private static final int M_CLOSEST = 25;
+	private static final Random rand = new Random(System.currentTimeMillis());
 
-	public static void main (String[] args) {
+	public static void main (String[] args)
+	{
 		List<Point> points = new ArrayList<Point>();
-		for (int i = 0; i < POINT_COUNT; i++) {
+		for (int i = 0; i < POINT_COUNT; i++)
 			points.add(newRandomPoint());
-		}
 
 		Point origin = newRandomPoint();
-		List<Point> closestPoints = findMClosest(origin, points, M_CLOSEST);
+		long start = System.currentTimeMillis();
 		List<Point> realClosestPoints = referenceMethod(origin, points, M_CLOSEST);
+		long twixt = System.currentTimeMillis();
+		List<Point> closestPoints = findMClosest(origin, points, M_CLOSEST);
+		long end = System.currentTimeMillis();
 
-		assert closestPoints.equals(realClosestPoints);
+		TreeSet<PointDistance> distanceTree = new TreeSet<PointDistance>();
+		for (Point point : points)
+			distanceTree.add(new PointDistance(point, distance(origin, point)));
+
+		System.out.println("Origin : " + origin.toString());
+		System.out.println("Closest " + M_CLOSEST + " Points");
+
+		System.out.println();
+		System.out.println("referenceMethod(), took " + (end - twixt) + " ms:");
+		for (Point point : realClosestPoints)
+			System.out.println("  " + point + " distance=" + distance(origin, point));
+
+		System.out.println();
+		System.out.println("findMClosest(), took " + (twixt - start) + " ms:");
+		for (Point point : closestPoints)
+			System.out.println("  " + point + " distance=" + distance(origin, point));
+
+		System.out.println();
+		System.out.println("Correct count? " + (closestPoints.size() == M_CLOSEST));
+		System.out.println("Matches reference? " + realClosestPoints.equals(closestPoints));
+
 	}
 
-	private static Point newRandomPoint() {
-			Point newPoint = new Point();
-			newPoint.x = (int) (Math.random() * 1000.0);
-			newPoint.y = (int) (Math.random() * 1000.0);
-			return newPoint;
+	private static Point newRandomPoint()
+	{
+		Point newPoint = new Point();
+		newPoint.x = (int) (rand.nextDouble() * 1000.0 - 500.0);
+		newPoint.y = (int) (rand.nextDouble() * 1000.0 - 500.0);
+		return newPoint;
 	}
 
-	private static List<Point> findMClosest(Point origin, List<Point> points, int M) {
-		return referenceMethod(origin, points, M);
-	}
+	private static List<Point> findMClosest(Point origin, List<Point> points, int M)
+	{
+		PriorityQueue<PointDistance> pointQueue = new PriorityQueue<PointDistance>();
+		for (Point point : points)
+		{
+			PointDistance pointDistance = new PointDistance(point, distance(origin, point));
+			pointQueue.add(pointDistance);
+		}
 
-	private static double distance(Point a, Point b) {
-		double x_squared = Math.pow(a.x - b.x, 2);
-		double y_squared = Math.pow(a.y - b.y, 2);
-		double sqrt_diff = Math.sqrt(x_squared + y_squared);
-		return sqrt_diff;
-	}
-
-	private static List<Point> referenceMethod(Point origin, List<Point> points, int M) {
 		List<Point> closestPoints = new ArrayList<Point>();
+		for (int i = 0; i < M; i++) {
+			closestPoints.add(pointQueue.remove().getPoint());
+		}
+
 		return closestPoints;
 	}
 
-	private static class PointDistance implements Comparable<PointDistance> {
-		Point point;
-		double distance;
 
-		public PointDistance(Point point, double distance) {
+	private static double distance(Point a, Point b)
+	{
+		double x_diff = a.getX() - b.getX();
+		double y_diff = a.getY() - b.getY();
+		double x_squared = x_diff * x_diff;
+		double y_squared = y_diff * y_diff;
+		return Math.sqrt(x_squared + y_squared);
+	}
+
+	private static List<Point> referenceMethod(Point origin, List<Point> points, int M)
+	{
+		TreeSet<PointDistance> pointTree = new TreeSet<PointDistance>();
+		for (Point point : points)
+		{
+			PointDistance pointDistance = new PointDistance(point, distance(origin, point));
+			if (pointTree.size() < M)
+			{
+				pointTree.add(pointDistance);
+			}
+			else if (pointTree.last().compareTo(pointDistance) > 0)
+			{
+				pointTree.pollLast();
+				pointTree.add(pointDistance);
+			}
+		}
+
+		List<Point> closestPoints = new ArrayList<Point>();
+		for (PointDistance pointDistance : pointTree)
+			closestPoints.add(pointDistance.getPoint());
+
+		return closestPoints;
+	}
+
+	private static class PointDistance implements Comparable<PointDistance>
+	{
+		private Point point;
+		private double distance;
+
+		public PointDistance(Point point, double distance)
+		{
 			this.point = point;
 			this.distance = distance;
 		}
 
+		public Point getPoint()
+		{
+			return point;
+		}
+
 		@Override
-		public int compareTo(PointDistance other) {
+		public int compareTo(PointDistance other)
+		{
 			if (distance < other.distance)
 				return -1;
 			if (distance > other.distance)
 				return 1;
+			if (this != other)
+				return 1;
 			return 0;
 		}
 
+		@Override
+		public boolean equals(Object other)
+		{
+			return this == other;
+		}
 	}
 }
