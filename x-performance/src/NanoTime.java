7e6a6f731a4e92af9ff1754bfd6508bde803591c
Joel Edwards <joeledwards@loadstorm.com>
2013-05-09 12:08:27 -0600

    Added NanoTime test class.

---
+++ x-performance/src/NanoTime.java
@@ -0,0 +1,12 @@
+
+public class NanoTime {
+	public static void main(String[] args) {
+		long totalTime = 0;
+		long startTime = System.nanoTime();
+		while (totalTime < 2000000000) {
+			long time = System.nanoTime();
+			totalTime = time - startTime;
+			System.out.println("time = " + time);
+		}
+	}
+}
