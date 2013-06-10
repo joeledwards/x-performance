e5bf788c555e46fb499a40a567cb039daf1f622a
Joel Edwards <joeledwards@loadstorm.com>
2013-06-10 17:16:38 -0600

    Added new test file for Charsets

---
+++ x-performance/src/CharsetTests.java
@@ -0,0 +1,11 @@
+
+public class CharsetTests
+{
+	public static void main(String[] args) {
+		String funnyName = "Вадим Якубец";
+		System.out.println(funnyName);
+
+		funnyName = "Нил Шарафутдинов";
+		System.out.println(funnyName);
+	}
+}
