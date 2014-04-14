d0abf391fa07619d7e19116a312b3838c8aae8a9
Joel Edwards <joeledwards@loadstorm.com>
2014-04-14 12:25:05 -0600

    Updated project to Java SE 8 Added two new tests

---
+++ x-performance/src/streams/SimpleStream.java
@@ -0,0 +1,31 @@
+package streams;
+
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class SimpleStream
+{
+	public static void main(String[] args)
+	{
+		TreeSet<Integer> integers = new TreeSet<>();
+		integers.add(7);
+		integers.add(9);
+		integers.add(11);
+		integers.add(1);
+		integers.add(6);
+		integers.add(2);
+		integers.add(1);
+		integers.add(6);
+		integers.add(13);
+		
+		System.out.println("Integers (original): " + integers);
+		
+		System.out.println("Integers (prefixed): " + 
+				String.join(", ",
+						integers.stream()
+								.map(i -> "Item-" + String.valueOf(i))
+								.collect(Collectors.toSet())
+				)
+		);
+	}
+}
---
+++ x-performance/src/structure/NullEnumInSwitch.java
@@ -0,0 +1,28 @@
+package structure;
+
+public class NullEnumInSwitch
+{
+	public static void main(String[] args)
+	{
+		Option option = null;
+		
+		switch(option)
+		{
+			case OPTION_A:
+				break;
+			case OPTION_B:
+				break;
+			case OPTION_C:
+				break;
+			default:
+				System.out.println("Supports null enum constant in switch!");
+		}
+	}
+	
+	public enum Option
+	{
+		OPTION_A,
+		OPTION_B,
+		OPTION_C
+	}
+}
